5b6673c400a364807a3dd19fe189141e286f60dd
Gabriel Andrade <gabrielgustavoandrade@gmail.com>
2023-07-15 15:40:16 -0300

    é nois

--- zapsalvo.py
+++ zapsalvo.py
@@ -3,33 +3,31 @@ import time
 import os
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import requests
-
-
+from selenium.webdriver.common.action_chains import ActionChains
 
 dir_path = os.getcwd()
-chrome_options2 = Options()
-chrome_options2.add_argument(r"user-data-dir=" + dir_path + "profile/zap")
-driver = webdriver.Chrome(chrome_options2)
+chrome_options = Options()
+chrome_options.add_argument("user-data-dir=" + dir_path + "/profile/zap")
+
+driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://web.whatsapp.com')
 time.sleep(10)
 
 def bot():
-
     try:
-        bolinha = driver.find_element(By.CLASS_NAME,'aumms1qt')
-        bolinha = driver.find_elements(By.CLASS_NAME,'aumms1qt')
+        bolinha = driver.find_elements(By.CLASS_NAME, 'aumms1qt')
         click_bolinha = bolinha[-1]
-        acao_bolinha = webdriver.common.actions_chains.ActionChains(driver)
-        acao_bolinha.move_to_element_with_offset(click_bolinha,0,-20)
-        acao_bolinha.click()
-        acao_bolinha.perform()
-        acao_bolinha.click()
-        acao_bolinha.perform()
+        
+        action = ActionChains(driver)
+        action.move_to_element_with_offset(click_bolinha, 0, -20)
+        action.click().perform()
+        action.click().perform()
     
-    except:
-        print('buscando novas notificações')
+    except IndexError:
+        print('No new notifications found')
+    
+    except Exception as e:
+        print('An error occurred:', str(e))
 
 while True:
     bot()
